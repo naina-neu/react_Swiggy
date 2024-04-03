fcd9c4c386f77743e942bfa5ad7ebf6865051bf9
Naina Rajan <rajan.nai@northeastern.edu>
2024-04-03 11:30:28 -0400

    Igniting our App - React using parcel as bundler

--- App.js
+++ App.js
@@ -11,16 +11,19 @@
  *
  */
 
+import React from "react";
+import ReactDOM from "react-dom";
+
 const parent = React.createElement(
   "div",
   { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", { id: "h1" }, "I'm an h1 tag"),
-    React.createElement("h2", { id: "h1" }, "I'm an h2 tag"),
+  React.createElement("div", { id: "child1", key: "child1" }, [
+    React.createElement("h1", { id: "h1", key: "h1" }, "I'm an h1 tag"),
+    React.createElement("h2", { id: "h12", key: "h2" }, "I'm an h2 tag"),
   ]),
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", { id: "h1" }, "I'm an h1 tag"),
-    React.createElement("h2", { id: "h1" }, "I'm an h2 tag"),
+  React.createElement("div", { id: "child2", key: "child2" }, [
+    React.createElement("h1", { id: "h13", key: "h3" }, "I'm an h1 tag"),
+    React.createElement("h2", { id: "h14", key: "h4" }, "I'm an h2 tag"),
   ])
 );
 
@@ -29,10 +32,11 @@ const heading1 = React.createElement(
   {
     id: "heading",
     XYZ: "abc",
+    key: "heading", // Adding key to heading1 element
   },
   "Hello World - React"
 );
 
-console.log(parent); // Object
-const root1 = ReactDOM.createRoot(document.getElementById("root1"));
-root1.render(parent);
+console.log(parent);
+const root1 = document.getElementById("root1");
+ReactDOM.render(parent, root1);
